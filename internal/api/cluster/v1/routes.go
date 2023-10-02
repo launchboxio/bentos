63f021a3e6700c4d3e35457ce687e21868d6c9ee
Robert Wittman <robkwittman@gmail.com>
2023-10-02 12:44:02 -0400

    Add base v1 routes, add development configs, add docker-compose stack

--- internal/api/cluster/v1/routes.go
+++ internal/api/cluster/v1/routes.go
@@ -6,10 +6,10 @@ import (
 )
 
 func AddRoutes(router *gin.RouterGroup) {
-	instanceGroup := router.Group("/instances/v1")
-	instanceGroup.GET("/health", func(context *gin.Context) {
+	clusterGroup := router.Group("/clusters/v1")
+	clusterGroup.GET("/health", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
-			"route": "/api/instances/v1",
+			"route": "/api/clusters/v1",
 		})
 	})
 }
--- internal/api/function/v1/routes.go
+++ internal/api/function/v1/routes.go
@@ -6,10 +6,10 @@ import (
 )
 
 func AddRoutes(router *gin.RouterGroup) {
-	instanceGroup := router.Group("/instances/v1")
-	instanceGroup.GET("/health", func(context *gin.Context) {
+	functionGroup := router.Group("/functions/v1")
+	functionGroup.GET("/health", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
-			"route": "/api/instances/v1",
+			"route": "/api/functions/v1",
 		})
 	})
 }
